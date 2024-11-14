d8e61b63f20bfe0e50bc23194ef2d916a1a4aeb3
suvakantamalik <134865642+suvakantamalik@users.noreply.github.com>
2024-11-14 16:06:45 +0530

    Update Main.java
    solve problem

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String[] args) {
+        RWOnly rwOnly = new RWOnly();
+
+//        try, setting a value to name, by directly accessing it using obj and also try printing it
+//        rwOnly.name = "Suva";
+//        'name' has private access in 'com. driver. RWOnly'
+
+        rwOnly.setName("Suva");
+        System.out.println(rwOnly.getName());
+
+    }
 }
